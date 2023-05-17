9abb95e6b3a0e01662914f0b2313a6a0f1653eb8
Michael <123170392+MichaelHoughtonDeBox@users.noreply.github.com>
2023-05-17 20:01:58 +0100

    Update create.py

--- create.py
+++ create.py
@@ -8,6 +8,7 @@ from  langchain import PromptTemplate, LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from streamlit_extras.switch_page_button import switch_page
+import ast
 
 api = st.secrets["OPENAI_KEY"]
 
